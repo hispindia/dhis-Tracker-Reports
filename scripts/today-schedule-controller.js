8e236188c944c4e0135817a211cfce733f2c8067
PriyankaBawa <priyankabawa609@gmail.com>
2016-12-15 16:35:34 +0530

    enrollment, event date fix

--- scripts/today-schedule-controller.js
+++ scripts/today-schedule-controller.js
@@ -22,14 +22,14 @@ msfReportsApp
                                             $timeout,
                                             MetadataService){
 
-       // const SQLVIEW_TEI_PS = "nBCleImsp8E";
-       // const SQLVIEW_TEI_ATTR = "NJKQr9q6kOO";
-       // const SQLVIEW_TEI_PS =  "EX2dsz6vmES";
-        // const SQLVIEW_TEI_ATTR = "pUiDfNYflvv";
 
-        const SQLVIEW_TEI_PS =  "FcXYoEGIQIR";
-         const SQLVIEW_TEI_ATTR = "WMIMrJEYUxl";
+//PSI
+        //const SQLVIEW_TEI_PS =  "FcXYoEGIQIR";
+        // const SQLVIEW_TEI_ATTR = "WMIMrJEYUxl";
 
+        //MSF
+        const SQLVIEW_TEI_PS =  "Ysi6iyNK1Ha";
+        const SQLVIEW_TEI_ATTR = "GoPX942y3eV";
         jQuery(document).ready(function () {
             hideLoad();
         })
@@ -214,6 +214,7 @@ msfReportsApp
                 var attrvalue = attrData.rows[i][index_attrvalue];
                 var ouname = attrData.rows[0][index_ouname];
                 var enrollDate = attrData.rows[i][index_enrollmentDate]; // enrollment date
+                enrollDate = enrollDate.substring(0, 10);
 
                 if (teiWiseAttrMap[teiuid] == undefined){
                     teiWiseAttrMap[teiuid] = [];
@@ -256,6 +257,7 @@ msfReportsApp
                 var psuid = stageData.rows[i][index_ps];
                 var evuid = stageData.rows[i][index_ev];
                 var evDate = stageData.rows[i][index_evDate];
+                evDate = evDate.substring(0, 10);
                 var deuid = stageData.rows[i][index_deuid];
                 var devalue = stageData.rows[i][index_devalue];
                 var ou = stageData.rows[i][index_ou];
